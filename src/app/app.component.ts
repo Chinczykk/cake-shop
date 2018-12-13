3d66104ae0dc9dc51ff0c62540ecc07a59524ebc
Marcin Imielski <mimielski@syncad.com>
2018-12-13 21:40:25 +0100

    iterate through items and show them on the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  public item = {
-    name: 'Ciasto truskawkowe',
-    desc: 'Ciasto z lekkim musem truskawkowym i bitą śmietaną na czekoladowym spodzie',
-    price: 40,
-    number_of_portions: 4,
-    image: ''
-  };
+  public items = [
+    {
+      name: 'Ciasto truskawkowe',
+      desc: 'Ciasto z lekkim musem truskawkowym i bitą śmietaną na czekoladowym spodzie',
+      price: 40,
+      number_of_portions: 4,
+      image: 'https://cdn.pixabay.com/photo/2017/10/12/13/13/cake-2844572_960_720.jpg'
+    }, {
+      name: 'Tor czekoladowy',
+      desc: 'Tor oblany mleczną czekoladą z orzechami',
+      price: 50,
+      number_of_portions: 8,
+      image: 'https://cdn.pixabay.com/photo/2017/03/14/05/49/small-cake-2142072_960_720.jpg'
+    }
+  ];
 
   constructor() {}
 
